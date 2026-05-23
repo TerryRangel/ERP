cfbe8ef2ee199539e5b6c82f4da50b1ac13d6f4c
Gabo9702 <gabo.rdz94@outlook.com>
2026-05-23 15:16:29 -0600

    Agregué filtro de búsqueda en página de productos y cree la página inventario funcional

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -9,6 +9,7 @@ import SuppliersPage from '../pages/Suppliers/SuppliersPage';
 import ProductsPage from '../pages/Products/ProductsPage';
 import ClientsPage from '../pages/Clients/ClientsPage'; 
 import NoAutorizado from '../components/layout/nopermission';
+import InventoryPage from '../pages/Inventory/InventoryPage';
 
 export default function AppRoutes() {
   return (
@@ -64,6 +65,15 @@ export default function AppRoutes() {
             element={
               <RequirePermission I="clients:read">
                 <ClientsPage />
+              </RequirePermission>
+            }
+          />
+
+          <Route 
+            path='/inventory'
+            element={
+              <RequirePermission I="inventory:read">
+                <InventoryPage />
               </RequirePermission>
             }
           />
